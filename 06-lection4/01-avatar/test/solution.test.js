9253b60945a7c338a289bb6a9222facc538aa3bf
t3131653 <107500245+t3131653@users.noreply.github.com>
2022-07-19 23:13:33 +0300

    Delete 06-lection4 directory

--- 06-lection4/01-avatar/test/solution.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('lection4/avatar', () => {
-  it('Аватарка', () => {
-    expect(1).to.equal(1);
-  });
-});
--- 06-lection4/02-background-image/test/solution.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('lection4/background-image', () => {
-  it('Фон', () => {
-    expect(1).to.equal(1);
-  });
-});
