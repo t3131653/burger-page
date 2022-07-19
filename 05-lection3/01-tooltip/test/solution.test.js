fb6d8b61b3326cf6bf18351824d1bb3a13ed8185
t3131653 <107500245+t3131653@users.noreply.github.com>
2022-07-19 23:13:21 +0300

    Delete 05-lection3 directory

--- 05-lection3/01-tooltip/test/solution.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('lection3/tooltip', () => {
-  it('Тултип', () => {
-    expect(1).to.equal(1);
-  });
-});
--- 05-lection3/03-calendar/test/solution.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('lection3/calendar', () => {
-  it('Календарь', () => {
-    expect(1).to.equal(1);
-  });
-});
