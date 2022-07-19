670aa15a99e10d4d02f07e21fcaf8e619b8c3708
t3131653 <107500245+t3131653@users.noreply.github.com>
2022-07-19 23:12:56 +0300

    Delete 03-lection1 directory

--- 03-lection1/01-button/test/solution.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('lection1/button', () => {
-  it('Кнопка', () => {
-    expect(1).to.equal(1);
-  });
-});
--- 03-lection1/02-accordion/test/solution.test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('lection1/accordion', () => {
-  it('Аккордеон', () => {
-    expect(1).to.equal(1);
-  });
-});
